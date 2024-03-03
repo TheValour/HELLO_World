a2ee8305ae827ac4091092ac7f30f56fa05b1489
TheValour <hellowroldexe18@gmail.com>
2024-03-03 12:06:51 +0530

    basic server setting change

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [`${process.env.FRONTEND_URL}`], 
+    origin: `${process.env.FRONTEND_URL}`, 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     optionSuccessStatus:200
